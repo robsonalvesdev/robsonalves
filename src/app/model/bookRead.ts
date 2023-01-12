973dee9457bbbba3a59474242e68758e222bf785
Robson Alves <robson.alves@consilux.com.br>
2023-01-12 17:05:20 -0300

    feat: subtitulo

--- src/app/model/bookRead.ts
+++ src/app/model/bookRead.ts
@@ -1,6 +1,8 @@
 export class BookRead {
     title: string = "";
+    subtitle: string = "";
     author: string[] = [];
+    editor: string = "";
     yearPublish: number = 0;
     tags: string[] = [];
     uri: string = ""
